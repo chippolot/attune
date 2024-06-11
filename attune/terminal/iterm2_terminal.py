0532921e4dac6658e083c7f6a33df98ab4614d96
Ben Smith <chippolot@gmail.com>
2024-06-11 13:18:42 -0700

    Applying opacity to terminal. Improving iterm2 restarting

--- attune/terminal/iterm2_terminal.py
+++ attune/terminal/iterm2_terminal.py
@@ -1,8 +1,11 @@
 import os
 import plistlib
 import subprocess
+import sys
 import time
 from pathlib import Path
+
+from attune.themes import get_theme_param
 
 
 class Terminal:
@@ -29,7 +32,7 @@ class Terminal:
             plistlib.dump(plist_data, f)
 
         # Restart iTerm2 to apply the changes
-        self._restart_iterm2()
+        self._restart()
 
     def set_theme(self, name, theme_path):
         plist_path = os.path.expanduser(
@@ -55,25 +58,53 @@ class Terminal:
         with open(theme_path, "rb") as f:
             theme_data = plistlib.load(f)
 
+        defaultTheme = plist_data["New Bookmarks"][0]
+
         for key, value in theme_data.items():
-            plist_data["New Bookmarks"][0][key] = value
-            plist_data["New Bookmarks"][0][key + " (Light)"] = value
-            plist_data["New Bookmarks"][0][key + " (Dark)"] = value
+            defaultTheme[key] = value
+            defaultTheme[key + " (Light)"] = value
+            defaultTheme[key + " (Dark)"] = value
 
         plist_data[color_preset_key][theme_name] = theme_data
-        plist_data["New Bookmarks"][0]["Dynamic Profiles"] = theme_name
+        defaultTheme["Dynamic Profiles"] = theme_name
+
+        value = get_theme_param(theme_name, "terminal.opacity")
+        if value is not None:
+            defaultTheme["Transparency"] = 1.0 - (value / 100.0)
 
         # Write the updated plist back to the file
         with open(plist_path, "wb") as f:
             plistlib.dump(plist_data, f)
 
         # Restart iTerm2 to apply the changes
-        self._restart_iterm2()
+        self._restart()
 
-    def _restart_iterm2(self):
-        # Kill iTerm2 if it is running
-        subprocess.run(["killall", "iTerm2"], stderr=subprocess.DEVNULL, check=False)
-        # Wait for a moment to ensure iTerm2 is closed
-        time.sleep(2)
-        # Open iTerm2
-        subprocess.run(["open", "-a", "iTerm"])
+    def _restart(self):
+        # Fork a child process
+        pid = os.fork()
+        if pid > 0:
+            # Parent process exits immediately
+            sys.exit()
+        else:
+            # Child process continues
+            # Create a new session to detach from the terminal
+            os.setsid()
+
+            # Fork again to ensure the process is re-parented to init (PID 1)
+            pid = os.fork()
+            if pid > 0:
+                # First child process exits
+                sys.exit()
+            else:
+                # Second child process continues
+                # Wait a moment to ensure the parent terminal is detached
+                time.sleep(0.75)
+
+                # Kill iTerm2
+                subprocess.run(["pkill", "iTerm2"])
+
+                # Wait for a moment to ensure the process is terminated
+                time.sleep(1)
+
+                # Restart iTerm2
+                subprocess.run(["open", "-a", "iTerm"])
